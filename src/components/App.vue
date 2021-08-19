b8e390fdaf0d49955d19111a545771a6f59a0873
kira-giz <pyscripthon0426@gmail.com>
2021-08-19 16:34:47 +0900

    add: childコンポーネントの追加

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,15 +2,20 @@
   <div>
     <h1>{{message}}</h1>
     <hr>
+    <ChildComponent></ChildComponent>
   </div>
 </template>
 
 <script>
+import ChildComponent from './ChildComponent';
 export default {
+  components: {
+    ChildComponent
+  },
   //es6のメソッド記法
   data() {
     return {
-      message: 'Hello Vue',
+    message: '<span>Hello Vue</span>',
     }
   }
 }
