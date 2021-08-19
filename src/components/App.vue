bfed677b043cf12b8d682f2c60882a6591392bb7
kira-giz <pyscripthon0426@gmail.com>
2021-08-19 17:56:06 +0900

    update: :keyについて

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <h1 v-html="message" :class="classObject"></h1>
-    <hr>
-    <ChildComponent v-show="isShow"></ChildComponent>
+    <hr />
+    <template v-for="item in items">
+      <ChildComponent v-show="isShow" :key="item.id"></ChildComponent>
+    </template>
     <p v-if="id === 12">1</p>
     <p v-else-if="id === 2">2</p>
     <template v-else-if="id === 3">
@@ -11,34 +13,55 @@
       <p>2-3</p>
     </template>
     <p v-else>other</p>
+    <ul>
+      <template v-for="item in items">
+        <li :key="item.id">
+          {{ item.title }}
+        </li>
+      </template>
+    </ul>
   </div>
 </template>
 
 <script>
-import ChildComponent from './ChildComponent';
+import ChildComponent from "./ChildComponent";
 export default {
   components: {
-    ChildComponent
+    ChildComponent,
   },
   //es6のメソッド記法
   data() {
     return {
-    message: '<span>Hello Vue</span>',
-    isShow: true,
-    id: 3,
-    classObject: {
-      'is-green': true
-    }
-    }
-  }
-}
+      message: "<span>Hello Vue</span>",
+      isShow: true,
+      id: 3,
+      classObject: {
+        "is-green": true,
+      },
+      items: [
+        {
+          id: this.$uuid.v4(),
+          title: "1番目のリスト",
+        },
+        {
+          id: this.$uuid.v4(),
+          title: "２番目のリスト",
+        },
+        {
+          id: this.$uuid.v4(),
+          title: "3番目のリスト",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .is-green {
-    color: green;
-  }
-  hr {
-    margin: 16px 0;
-  }
+.is-green {
+  color: green;
+}
+hr {
+  margin: 16px 0;
+}
 </style>
